61a54c85b8ba73bb9d596104e8abc4374e412428
Cloud User <ec2-user@ip-172-31-14-89.us-east-2.compute.internal>
2017-04-25 22:12:09 -0400

    cant add more than one nick, check our the list != null loop

--- profanity.c
+++ profanity.c
@@ -8,37 +8,42 @@
 #define RECV_BUF_LEN 1024
 
 
+    struct names {char *nick; int count; struct names *next;};
+    struct names *list;
+    char *profanity_handler_function(char *,char *); 
+
+
 int main(){
 
-    char input[31];
-    char *test = "HELLO WORLD";
-    char *new;
-    char var[20];
-    strcpy(var, test);
-        	
-    printf("\n%s\n", var);
-
-    for(new = var; *new != '\0'; new++){
-
-       *new = tolower(*new);
-    }
-
-    printf("\n%s\n", var);
-
-    if(strstr(var, "hello") != NULL){
-
-       printf("\nIT WORKS\n");
-    } 
-
+    
+    char *word="shit";
+    char *nick="Bob";
+    char *nnick="tom";
+    char *nword="~profanity";
+    char *back;
+    
+     	
+    back = profanity_handler_function(nick, word);
+    printf("\n%s\n",back);
+    back = profanity_handler_function(nick, word);
+    printf("\n%s\n",back);   
+    /*back = profanity_handler_function(nnick, word);
+    printf("\n%s\n",back);
+    back = profanity_handler_function(nnick, word);
+    printf("\n%s\n",back);
+*/
+    back = profanity_handler_function(nick, nword);
+     
+ 
     return 0;
 }
 
-char *profanity_handler_function(char *nick, const char *message) {
-    struct names {char *nick; int count; struct names *next;};
-
+char *profanity_handler_function(char *nick, char *message) {
+    struct names *keep;
+    keep = (struct names *)malloc(sizeof(struct names));
     char *ret = NULL;
     char *newm;
-    char *line = "CNIT BOT is a family friendly bot, please no explicit language";
+    char *line = "CNIT BOT is a family friendly bot, please no explicit language\n";
     char tmp[100];
     strcpy(tmp,message);
 
@@ -47,12 +52,52 @@ char *profanity_handler_function(char *nick, const char *message) {
     }
 
     if (strstr(tmp, "fuck") != NULL || strstr(tmp, "shit") != NULL ||
-        strstr(tmp, "ass") != NULL || strstr(tmp, "bitch") != NULL ||
+        strstr(tmp, " ass ") != NULL || strstr(tmp, "bitch") != NULL ||
         strstr(tmp, "damn") != NULL) {
+ 
+	if(list!=NULL){
+	keep=list;
+        while(list != NULL){  
 
+        if(strcmp(list->nick,nick) == 0){
+
+	list->count = list->count+1; 
+        break; 
+        } 
+	
+	list=list->next;
+	}
+	list=keep;
+
+	/*struct names *temp;
+	temp  = (struct names *)malloc(sizeof(struct names));
+	strcpy(temp->nick,nick);
+	temp->count = 1;
+	temp->next = list;
+	list = temp;*/
+	}
+
+	if(list == NULL){
+	struct names *quick;
+	quick = (struct names *)malloc(sizeof(struct names));
+	quick->nick=nick;
+	quick->count=1;
+	quick->next=NULL;
+        list = quick;
+	}
+        
         ret = malloc(512);
         strcpy(ret, line);
-
+    }
+    if(strstr(tmp,"~profanity") != NULL){
+	struct names *print;
+	print = (struct names *)malloc(sizeof(struct names));
+	print = list;
+	while(list != NULL){
+	printf("Name: %s Count: %d\n",list->nick,list->count);
+	list=list->next;
+	}
+        list=print;
     }
     return ret;
 }
